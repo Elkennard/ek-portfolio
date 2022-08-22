0faea3264bf5c2ede798ab1c7d10741d771bf5b6
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-08-22 20:29:18 +0100

    moved menu to the right and added job title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,11 +6,21 @@ function App() {
     <div className="App">
       <div className="nav">
         <h1>Emma Kennard</h1>
+        <h2>Junior Developer</h2>
         <nav activeclassname="active">
-          <NavLink className="inactive" to="/">Home</NavLink>{"  "}
-          <NavLink className="inactive" to="/Portfolio">Portfolio</NavLink>{" "}
-          <NavLink className="inactive" to="/Career">Career</NavLink>{" "}
-          <NavLink className="inactive" to="/Contact">Contact</NavLink>
+          <NavLink className="inactive" to="/">
+            Home   |
+          </NavLink>
+          {"  "}
+          <NavLink className="inactive" to="/Portfolio">
+            Portfolio   |
+          </NavLink>{" "}
+          <NavLink className="inactive" to="/Career">
+            Career   |
+          </NavLink>{" "}
+          <NavLink className="inactive" to="/Contact">
+            Contact
+          </NavLink>
         </nav>
         <Outlet />
       </div>
