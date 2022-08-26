d1fc3405dbcbc17e1c17e2c17238b06568b38c58
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-08-26 20:15:34 +0100

    created contact form linked with netlify and logos linked to linkedIn & GitHub

--- portfolio/src/routes/Contact/Contact.js
+++ portfolio/src/routes/Contact/Contact.js
@@ -1,9 +1,34 @@
-import "./Contact.css"
+import "./Contact.css";
 
 export default function Contact() {
-    return (
-      <main>
-        <h3>Contact - Under construction</h3>
-      </main>
-    );
-  }
+
+  const images = {
+    linkedIn: require("../../Assets/LinkedIn.png"),
+    gitHub: require("../../Assets/GitHub.png")
+  };
+  return (
+    <div className="Contact">
+      <form
+        method="POST"
+        name="contactform"
+        className="contactForm"
+        action="/confirmation-page"
+      >
+        <input type="hidden" name="form-name" value="contactForm" />
+
+        <input type="text" name="name" placeholder="Name"/>
+
+        <input type="email" name="email" placeholder="Email" />
+
+        <textarea name="message" placeholder="Message"></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
+      <div className="logos">
+      <a href="https://www.linkedin.com/in/emma-kennard/" target="_blank" rel="noreferrer">
+      <img src={images.linkedIn} alt="LinkedIn Link" className="linkedIn"></img></a>
+      <a href="https://github.com/Elkennard" target="_blank" rel="noreferrer"><img src={images.gitHub} alt="GitHub Logo" className="gitHub Link"></img></a>
+      </div>
+    </div>
+  );
+}
