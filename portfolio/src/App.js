d2f988c88d7802e5edeaa957ebd8f8c0cedaa7ed
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-09-08 13:46:20 +0100

    arranged layout so columns convert to rows on smaller devices and paint splash is behind text

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,9 @@ import "./App.css";
 import { NavLink, Outlet } from "react-router-dom";
 
 function App() {
-
   const images = {
     navPaint: require("./Assets/paintStripe.png"),
     logo: require("./Assets/EKLogoSide.png"),
-    largePaint: require("./Assets/LargePaint.png"),
   };
 
   return (
@@ -29,14 +27,12 @@ function App() {
         </nav>
         <Outlet />
       </div>
-      <img src={images.logo} alt="Emma Kennard Logo" className="logo"/>
-      <img src={images.navPaint} alt="navigation decoration" className="navPaint"/>
+      <img src={images.logo} alt="Emma Kennard Logo" className="logo" />
       <img
-            src={images.largePaint}
-            alt="Paint Splash"
-            className="largePaint"
-          />
-      
+        src={images.navPaint}
+        alt="navigation decoration"
+        className="navPaint"
+      />
     </div>
   );
 }
--- portfolio/src/routes/Home/Home.js
+++ portfolio/src/routes/Home/Home.js
@@ -3,18 +3,22 @@ import "./Home.css";
 export default function Home() {
   const images = {
     headshot: require("../../Assets/ProfileEK.png"),
+    largePaint: require("../../Assets/LargePaint.png"),
   };
 
   return (
     <main>
       <div className="intro">
         <img src={images.headshot} alt="Emma Headshot" className="introImage" />
+        <h3 className="introHeader">Hi, I'm Emma ...</h3>
         <p className="introText">
-          “Hi, I'm Emma, for the last 25 years I've been sourcing and collating venues for
+          For the last 25 years I've been sourcing and collating venues for
           corporate events. I was ready for a new challenge and was fascinated
           when I liaised with developers on projects. Now I'm sourcing and
-          collating code myself!”
+          collating code myself!
         </p>
+        <img src={images.largePaint} alt="Paint Splash" className="largePaint" />
+        
       </div>
     </main>
   );
