74d2d13acf6a1172e429241ac52e600a9aaf117e
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-09-15 13:58:58 +0100

    split contact page into columns

--- portfolio/src/routes/Contact/Contact.js
+++ portfolio/src/routes/Contact/Contact.js
@@ -1,27 +1,47 @@
 import "./Contact.css";
 
 export default function Contact() {
-
   return (
-    <div className="contact">
-    <h3 className="contact_header">Drop me a line</h3>
-    <p className="contact_content">I'd love to hear from you. Use one of the social media links or complete the form and I'll get back to you.</p>
-      <form
-        method="POST"
-        name="contactform"
-        className="contact"
-        action="/confirmation-page.html"
-      >
-        <input type="hidden" name="form-name" value="contactForm" />
+    <div className="contact_wrapper">
+      <div className="contact_text">
+            <h3 className="contact_header">Drop me a line</h3>
+      <p className="contact_content">
+        I'd love to hear from you. Use one of the social media links or complete
+        the form and I'll get back to you.
+      </p>
+      </div>
+        <form
+          method="POST"
+          name="contactform"
+          className="contact"
+          action="/confirmation-page"
+        >
+          <input type="hidden" name="form-name" value="contactForm" />
 
-        <input type="text" className="contact_name" name="name" placeholder="Name"/>
+          <input
+            type="text"
+            className="contact_name"
+            name="name"
+            placeholder="Name"
+          />
 
-        <input type="email" className="contact_email" name="email" placeholder="Email" />
+          <input
+            type="email"
+            className="contact_email"
+            name="email"
+            placeholder="Email"
+          />
 
-        <textarea className="contact_message" name="message" placeholder="Message"></textarea>
+          <textarea
+            className="contact_message"
+            name="message"
+            placeholder="Message"
+          ></textarea>
 
-        <button type="submit" className="contact_button">Submit</button>
-      </form>
+          <button type="submit" className="contact_button">
+            Submit
+          </button>
+        </form>
     </div>
   );
 }
