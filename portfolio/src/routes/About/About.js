0f1d839a5d3e99e3b74aab8e56dd920600dc8c12
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-09-15 17:24:34 +0100

    added tech stack images

--- portfolio/src/routes/About/About.js
+++ portfolio/src/routes/About/About.js
@@ -1,9 +1,163 @@
-import "./About.css"
+import "./About.css";
 
 export default function About() {
-    return (
-      <main>
-        <h3>About - Under construction</h3>
-      </main>
-    );
-  }
+  return (
+    <main className="About">
+      <div className="about_text_wrapper">
+        <h3 className="about_header">A little about me</h3>
+        <p className="about_text">Under construction</p>
+      </div>
+      
+      <div className="Tech">
+      <h3 className="tech_header">Tech I've experinced so far;</h3>
+        <a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://svgarchive.com/wp-content/uploads/react-native.svg"
+            alt="React-Native"
+          />
+        </a>
+        <a href="https://beta.reactjs.org/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg"
+            alt="React"
+          />
+        </a>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+          target="_blank" rel="noreferrer"
+        >
+          <img className="tech_img"
+            src="https://seeklogo.com/images/H/html5-logo-EF92D240D7-seeklogo.com.png"
+            alt="HTML5"
+          />
+        </a>
+        <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://profilinator.rishav.dev/skills-assets/css3-original-wordmark.svg"
+            alt="CSS3"
+          />
+        </a>
+        <a href="https://www.javascript.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://profilinator.rishav.dev/skills-assets/javascript-original.svg"
+            alt="JavaScript"
+          />
+        </a>
+        <a href="https://github.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://profilinator.rishav.dev/skills-assets/git-scm-icon.svg"
+            alt="Git"
+          />
+        </a>
+        <a href="https://docs.expo.dev/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/expoio/expoio-ar21.svg"
+            alt="Expo"
+          />
+        </a>
+        <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-128.png"
+            alt="Firebase"
+          />
+        </a>
+        <a href="https://auth0.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://seeklogo.com/images/A/auth0-logo-700A01EF89-seeklogo.com.png"
+            alt="Auth0"
+          />
+        </a>
+        <a href="https://aws.amazon.com/?nc2=h_lg" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://seeklogo.com/images/A/amazon-web-services-aws-logo-6C2E3DCD3E-seeklogo.com.png"
+            alt="aws"
+          />
+        </a>
+        <a href="https://babeljs.io/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://d33wubrfki0l68.cloudfront.net/7a197cfe44548cc1a3f581152af70a3051e11671/78df8/img/babel.svg"
+            alt="Babel"
+          />
+        </a>
+        <a href="https://nodejs.org/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/nodejs/nodejs-horizontal.svg"
+            alt="Node.js"
+          />
+        </a>
+        <a href="https://expressjs.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://svg2raster.fileformat.info/vlz.jsp?svg=%2Flogos%2Fexpressjs%2Fexpressjs-ar21.svg"
+            alt="Express.js"
+          />
+        </a>
+        <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://profilinator.rishav.dev/skills-assets/postgresql-original-wordmark.svg"
+            alt="PostgreSQL"
+          />
+        </a>
+        <a href="https://heroku.com" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://github.com/jalbertsr/logo-badge-images/blob/master/img/rsz_heroku.png?raw=true"
+            alt="heroku"
+          />
+        </a>
+        <a href="https://jestjs.io" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+            alt="jest"
+          />
+        </a>
+        <a href="https://www.cypress.io/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://i0.wp.com/blog.knoldus.com/wp-content/uploads/2022/04/cypress.png?w=364&ssl=1"
+            alt="Cypress"
+          />
+        </a>
+        <a href="https://postman.com" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+            alt="postman"
+          />
+        </a>
+        <a href="https://reactnavigation.org/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://res.cloudinary.com/practicaldev/image/fetch/s--pY3vUQFV--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/ju7oq0dxqhaz1wpm1mv1.png"
+            alt="react navigation"
+          />
+        </a>
+        <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/netlify/netlify-ar21.svg"
+            alt="Netlify"
+          />
+        </a>
+        <a href="https://trello.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://www.vectorlogo.zone/logos/trello/trello-ar21.svg"
+            alt="Trello"
+          />
+        </a>
+        <a href="https://miro.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://cdn.worldvectorlogo.com/logos/miro-2.svg"
+            alt="Miro"
+          />
+        </a>
+        <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://seeklogo.com/images/F/figma-logo-E4E21D3AEA-seeklogo.com.png"
+            alt="figma"
+          />
+        </a>
+        <a href="https://www.piskelapp.com/" target="_blank" rel="noreferrer">
+          <img className="tech_img"
+            src="https://msbassetttech.weebly.com/uploads/1/3/3/9/13391031/piksel-logo_orig.png"
+            alt="Piskel"
+          />
+        </a>
+      </div>
+    </main>
+  );
+}
